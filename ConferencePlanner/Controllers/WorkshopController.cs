f26c193d29622c0a486ba179bd411714cffcebc4
Pavan Kumar <pavannayakiict@gmail.com>
2022-01-06 08:07:12 +0530

    Add project files.

---
+++ ConferencePlanner/Controllers/WorkshopController.cs
@@ -0,0 +1,34 @@
+﻿using ConferencePlanner.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConferencePlanner.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WorkshopController : ControllerBase
+    {
+        private ApplicationDbContext _context;
+        public WorkshopController(ApplicationDbContext context)
+        {
+
+            _context = context;
+            if (!_context.Workshops.Any())
+            {
+                _context.Workshops.Add(new Workshop
+                { Name = "Event Management", Speaker = "Shweta" });
+                _context.SaveChanges();
+            }
+        }
+
+        [HttpGet]
+        public IEnumerable<Workshop> GetWorkshops()
+        {
+            return _context.Workshops;
+        }
+    }
+}
---
+++ ConferencePlanner/Model/Workshop.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConferencePlanner.Model
+{
+    public class Workshop
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Speaker { get; set; }
+    }
+}
